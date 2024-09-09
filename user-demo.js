0546b79223ec34561bb885c121cacee2678619d0
Hyobin <116796285+dohb128@users.noreply.github.com>
2024-09-09 12:26:20 +0900

    Add login api
    로그인 api 추가

--- user-demo.js
+++ user-demo.js
@@ -11,7 +11,34 @@ let users = new Map()
 
 //POST - 로그인
 app.post('/login', (req, res) => {
-    const user = req.body
+    const {userId, userPw} = req.body
+
+    /*
+    users.forEach((v, k) => {
+        // a: value. b: key, c: Map
+        console.log(`a: ${a}, b: ${b}, c: ${c}`)
+    })
+    */
+
+
+    // users에 id가 존재하는지 확인
+    if (users.has(userId)) {  
+        const user = users.get(userId);  // 존재하면 해당 회원 정보 찾아서 user에 저장
+
+        if (user.userPw === userPw) { // 비밀번호 확인
+            res.status(200).json({
+                message: `${user.userName}님 환영합니다!`,
+            });
+        } else {    
+            res.status(401).json({
+                message: "비밀번호가 올바르지 않습니다.",
+            });
+        }
+    } else {    // 존재하지 않으면 없는 ID
+        res.status(404).json({
+            message: "존재하지 않는 ID입니다.",
+        });
+    }
 })
 
 //POST - 회원가입
