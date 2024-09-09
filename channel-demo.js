afd471f998acf244a7d87eba5a6eb09e97411d93
Hyobin <116796285+dohb128@users.noreply.github.com>
2024-09-10 00:56:34 +0900

    Upload channel-demo.js
    채널 api 업로드

---
+++ channel-demo.js
@@ -0,0 +1,104 @@
+// express 모듈 세팅
+const express = require('express')
+const app = express()
+app.listen(7777)
+
+//json 모듈 설정
+app.use(express.json())
+
+//채널 데이터 저장 
+let channels = new Map()
+let id = 1
+
+//POST - 채널 생성
+app.post('/channels', (req, res) => {
+    const channel = req.body
+
+    if (channel.channelTitle) {
+        channels.set(id++, channel)
+
+        res.status(201).json({
+            message: `${channel.channelTitle} 님, 유튜브 채널 개설을 축하드립니다!`
+        })
+    } else {
+        res.status(400).json({
+            message: "요청 값을 제대로 보내주세요."
+        })
+    }
+})
+
+//PUT - 개별 수정
+app.put('/channels/:id', function (req, res) {
+    let { id } = req.params
+    id = parseInt(id)
+
+    var channel = channels.get(id)
+
+    if (channel != undefined) {
+        var oldTitle = channel.channelTitle
+        var newTitle = req.body.channelTitle
+
+        channel.channelTitle = newTitle
+        channels.set(id, channel)
+
+        res.status(200).json({
+            message: `${oldTitle}님, 채널명이 ${newTitle}로 변경 되었습니다.`
+        })
+    } else {
+        res.status(404).json({
+            message: `요청하신 ${id}번은 존재하지 않는 채널입니다.`
+        })
+    }
+})
+
+//DELETE - 채널 삭제
+app.delete('/channels/:id', function (req, res) {
+    let { id } = req.params
+    id = parseInt(id)
+
+    var channel = channels.get(id)
+    if (channel != undefined) {
+        const channelTitle = channel.channelTitle
+        channels.delete(id)
+
+        res.status(200).json({
+            message: `${channelTitle}님 다음에 또 뵙겠습니다.`
+        })
+    } else {
+        res.status(404).json({
+            message: `요청하신 ${id}번은 존재하지 않는 채널입니다.`
+        })
+    }
+})
+
+
+//GET - 전체 조회
+app.get("/channels", function (req, res) {
+    var channelList = {}
+    if (channels.size !== 0) {
+        channels.forEach(function (value, key) {
+            channelList[key] = value
+        })
+
+        res.status(200).json(channelList)
+    } else {
+        res.status(404).json({
+            message: "등록된 채널이 없습니다."
+        })
+    }
+})
+
+//GET - 개별 조회
+app.get('/channels/:id', function (req, res) {
+    let { id } = req.params
+    id = parseInt(id)
+
+    const channel = channels.get(id)
+    if (channel) {
+        res.status(200).json(channel)
+    } else {
+        res.status(404).json({
+            message: "채널 정보를 찾을 수 없습니다."
+        })
+    }
+})
